964095f3c0ffcdb4e31839d4d8c25fb623276f44
glop102 <alexlikeschess@gmail.com>
2024-05-08 04:01:37 -0400

    proof of concept of the UI addon
    This is a great milestone of having a server working with addons
    dynamically registering endpoints to serve up the static content files
    that are in the module.
    
    - serves index.html as a static path and static filepath
    - serves javascript files from a static location with proper path
      normalization and checking to ensure no simple .. escapement
    - Basic webpage with a quick html template to translate the json into
      something a bit more human readable.
    
    It is unclear if i want to continue down the html template path that i
    have started in the html file, but that is totally fine! I can play with
    the front end as much as I want, and it is all contained to this addon
    to ensure I don't accidentally mess up core code, and that I never
    introduce dependencies to the core from the front end.

--- builtin_addons/html/__init__.py
+++ builtin_addons/html/__init__.py
@@ -1,11 +1,47 @@
 # Serves static HTML pages, static JS scripts, and also HTMX endpoints
 
+import pathlib
+
 from fastapi import Response,status,APIRouter
 from fastapi.responses import HTMLResponse
 
+import pipeline_backend
+
+root_file_folder = pathlib.Path(__file__).absolute().parent / "files"
+
 router = APIRouter()
 
-@router.get("/")
+@router.get("/",response_class=HTMLResponse)
+@router.get("/index.html", response_class=HTMLResponse)
 def get_homepage_file():
-    print(__file__)
-    return HTMLResponse("debug sucessful!")
+    try:
+        f = open(root_file_folder / "index.html")
+    except FileNotFoundError:
+        return HTMLResponse("Unable to find the UI File",status_code=status.HTTP_404_NOT_FOUND)
+    return f.read()
+
+@router.get("/js/{filename}")
+def get_javascript_file(filename:str):
+    js_folder = root_file_folder / "js"
+    req_js_path = (js_folder / filename).resolve()
+    if not js_folder in req_js_path.parents:
+        return Response("Forbidden",status_code=status.HTTP_403_FORBIDDEN)
+    if not req_js_path.exists():
+        return Response("Unable to find the File", status_code=status.HTTP_404_NOT_FOUND)
+    if not req_js_path.is_file():
+        return Response("Forbidden", status_code=status.HTTP_403_FORBIDDEN)
+    f = open(req_js_path)
+    return Response(f.read())
+
+@router.get("/image/{filename}")
+def get_image_file(filename: str):
+    image_folder = root_file_folder / "image"
+    req_image_path = (image_folder / filename).resolve()
+    if not image_folder in req_image_path.parents:
+        return Response("Forbidden", status_code=status.HTTP_403_FORBIDDEN)
+    if not req_image_path.exists():
+        return Response("Unable to find the File", status_code=status.HTTP_404_NOT_FOUND)
+    if not req_image_path.is_file():
+        return Response("Forbidden", status_code=status.HTTP_403_FORBIDDEN)
+    f = open(req_image_path)
+    return Response(f.read())
