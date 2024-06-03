6db0f6f3ce5dcd512722c757e1e9b257dd936056
glop102 <alexlikeschess@gmail.com>
2024-06-03 01:41:17 -0400

    Workflow Procedures list alternates coloring for redability

--- builtin_addons/html/node_project/src/components/Procedure.vue
+++ builtin_addons/html/node_project/src/components/Procedure.vue
@@ -21,7 +21,7 @@ const props = defineProps({
     <div class="procedure_details">
         <div class="procedure_details_name">{{ name }}</div>
         <div class="procedure_details_commands">
-            <Command v-for="c in commands" :command="c" />
+            <Command v-for="(c,idx) in commands" :command="c" :class="{'list_odd_item':idx%2==1}" />
         </div>
     </div>
 </template>
